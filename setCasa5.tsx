ea36d2e303ec5cb0106a7d8f53cea282652c75ae
Elray02 <tassofede@gmail.com>
2021-01-25 21:47:11 +0000

    Deploying to gh-pages from master @ cae3a32d28afb0d0718c8f8fb8c4ad6834a90562 🚀

--- setCasa5.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="setCasa5" tilewidth="32" tileheight="32" tilecount="621" columns="27">
- <image source="../../Downloads/nuevos_tiles_interiores__actualizables__pokemon_by_pablus94_ddzim2k.png" width="864" height="736"/>
-</tileset>
